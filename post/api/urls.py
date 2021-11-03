baed10e7d88cb229304fd0709b538a3943ff7813
eheperson <barannkuzu@gmail.com>
2021-11-03 12:17:57 +0300

    simple caching example included

--- post/api/urls.py
+++ post/api/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 from . import views
 
+from django.views.decorators.cache import cache_page
+
 app_name = 'post'
 # if we want to use 'namespace' keyword in our core/urls.py
 # we need to define app_name here
 
 urlpatterns = [
-    path('list/', views.PostListAPIView.as_view(), name="post-list"),
+    # path('list/', views.PostListAPIView.as_view(), name="post-list"),
+    path('list/', cache_page(60*1)(views.PostListAPIView.as_view()), name="post-list"), # caching post list api view
     path('list-model/', views.PostListAPIViewModel.as_view(), name="post-list-model"),
     path('detail/<slug>', views.PostDetailAPIView.as_view(), name="post-detail"),
     path('delete/<slug>', views.PostDeleteAPIView.as_view(), name="post-delete"),
